c87ad3e9825c73df4e74da1fa2a0c4f2ff07dc26
tarunshrestha <128881726+tarunshrestha@users.noreply.github.com>
2024-03-09 19:45:35 +0545

    cart_page

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,4 +22,5 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
